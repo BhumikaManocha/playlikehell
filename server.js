9abec275286cf20ee2ef0516a085362508c23434
BhumikaManocha <bhumika.manocha@gmail.com>
2019-11-28 01:38:09 +0530

    database connected

--- server.js
+++ server.js
@@ -1,12 +1,60 @@
+// if(process.env.NODE_ENV !== 'production'){
+    require('dotenv');
+// }
+
 const express = require('express');
 const app = express();
 const bcrypt = require('bcrypt');
+const passport = require('passport');
+const flash = require('express-flash');
+const session = require('express-session');
+const mongoose = require('mongoose');
+// mongoose.set('useNewUrlParser', true);
+// mongoose.set('useUnifiedTopology', true);
 
-const users = [];
+// const MongoClient = require('mongodb').MongoClient;
+// const uri = "mongodb+srv://bhumika:bhumi3699@my-first-cluster-p64nb.mongodb.net/test?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true });
+// client.connect(err => {
+//     console.log(err);
+//   const collection = client.db("test").collection("devices");
+//   console.log((collection));
+//   // perform actions on the collection object
+//   client.close();
+// });
+
+
+mongoose
+  .connect(
+    "mongodb+srv://bhumika:bhumi3699@my-first-cluster-p64nb.mongodb.net/test?retryWrites=true&w=majority"  )
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log("error -----",err));
+
+
+
+// const initializePassport = require('./passport-config')
+// initializePassport(
+//     passport,
+//     email => users.find(user => user.email === email),
+//     id => users.find(user => user.id === id)
+// )
+
+// const users = [];
+
+
 
 app.set('view engine', 'hbs');
 
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({extended:true}));
+app.use(flash());
+app.use(session({
+    secret:'asd',
+    // resave:false,
+    saveUninitialized:true
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+
 
 // app.use(express.static('play/public'));
 // app.use(express.static(path.resolve('./public')));
@@ -25,34 +73,35 @@ app.get('/register',(req,res) => {
     res.render('register')
 })
 
-app.post('/register', async(req,res) => {
-    try{
-        const hashedpassword = await bcrypt.hash(req.body.password,10);
-        users.push({
-            name : req.body.name,
-            gamingname :req.body.gamingname,
-            dateofbirth : req.body.dateofbirth,
-            email : req.body.email,
-            password: hashedpassword
-        })
-        res.redirect('/login');
-    }
-    catch{
-        res.redirect('/regsiter');
-    }
-    console.log("done")
-    console.log(users);
+// app.post('/register', async(req,res) => {
+//     try{
+//         const hashedpassword = await bcrypt.hash(req.body.password,10);
+//         users.push({
+//             name : req.body.name,
+//             gamingname :req.body.gamingname,
+//             dateofbirth : req.body.dateofbirth,
+//             email : req.body.email,
+//             password: hashedpassword
+//         })
+//         res.redirect('/login');
+//     }
+//     catch(err){
+//         res.redirect('/register');
+//     }
+//     console.log("done")
+//     console.log(users);
 
-})
+// })
 
 app.get('/login',(req,res) => {
     res.render('login')
 })
 
-app.post('/login',(req,res) => {
-
-
-})
+app.post('/login',passport.authenticate('local',{
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true
+}))
 
 
 
@@ -68,9 +117,9 @@ app.get('/puzzle',(req,res)=>{
     res.render('puzzle')
 })
 
-app.get('/quiz',(req,res)=>{
-    res.render('quiz')
-})
+// app.get('/quiz',(req,res)=>{
+//     res.render('quiz')
+// })
 
 app.get('/hypnotizer',(req,res)=>{
     res.render('hypnotizer')
